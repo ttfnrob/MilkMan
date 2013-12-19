29e934764dd8a325306f2862428717f8b1556a81
Robert Simpson <ttfnrob@gmail.com>
2013-12-19 22:30:20 +0000

    DBSCanning like a boss

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'rails', '3.2.5'
 gem "mongo_mapper"
 gem "bson_ext"
 
+gem "dbscan"
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- lib/dbscan/first_try.rb
+++ lib/dbscan/first_try.rb
@@ -1,57 +1,37 @@
-$:.unshift File.dirname($0)
-require 'subclu'
+require 'dbscan'
 require 'csv'
 
-EPS = 0.02
-MIN_PTS = 2
+EPS = 1
+MIN_PTS = 3
 
-db = [
-		[0.0,0.1,0.2,0.3],
-		[0.0,0.1,0.2,0.41],
-		[0.0,0.1,0.2,0.42],
-		[0.0,0.1,0.2,0.43],
-		[0.0,0.1,0.2,0.44],
-		[0.5,0.1,0.2,0.45],
-		[0.5,0.1,0.2,0.46],
-		[0.5,0.1,0.2,0.47],
-		[0.5,0.1,0.2,0.48],
-		[0.5,0.1,0.2,0.49],
-		[0.5,0.1,0.2,0.50],
-		[0.5,0.1,0.2,0.51],
-		[0.5,0.1,0.2,0.52]
-	]
-
-db = []
-object = "bowshock"
+points = []
+object = "galaxy"
 file = Dir.glob(Rails.root.to_s+"/data/raw/annotations/"+object+"*.csv").max_by {|f| File.mtime(f)}
 puts "Reading in data for type=#{object}"
+items=0
 CSV.foreach(file, :headers => true) do |row|
-  db << [ row["lat"], row["lon"], row["rx"], row["ry"] ] if object.in?('bubble', 'galaxy', 'ego', 'cluster')
-  db << [ row["lat"], row["lon"] ] if object.in?('pillars', 'bowshock', 'artifact', 'other')
+  items+=1
+  points << [ row["lat"].to_f, row["lon"].to_f, row["rx"].to_f, row["ry"].to_f ] if object.in?('bubble', 'galaxy', 'ego', 'cluster')
+  points << [ row["lat"].to_f, row["lon"].to_f, row["width"].to_f, row["height"].to_f ] if object.in?('pillars', 'bowshock', 'artifact', 'other')
 end
-puts "Data loaded from CSV."
+puts "#{items} items loaded from CSV."
 
-#dbscan runs on all attributes
-all_attribute_indices = db[0].index
-dbscan = DBscan.new(EuclideanDistance.new(all_attribute_indices))
+#Run DBSCAN
+dbscan = Clusterer.new( points, {:min_points => MIN_PTS, :epsilon => EPS})
 puts "DBSCAN initiated, processing..."
-result = dbscan.run(db, EPS, MIN_PTS)
-
-# #test run of subclu, which outputs all possibilities for projected clustering
-# subclu = SUBCLU.new(JensenShannonDistance)
-# result = subclu.run(db, EPS, MIN_PTS)
 
 puts "Writing data to output file."
 CSV.open( Rails.root.to_s+"/data/reduced/dbscan/"+object+"_"+DateTime.now.to_s(:number)+".csv", 'w') do |writer|
-	result.each_index do |dim|
-		result[4].each_pair do |subspace, clusters|
-			# puts "		#{subspace.inspect}"
-			clusters.each do |cluster|
-					writer << cluster
-				# puts "			#{cluster}"
-			end		
-		end
+	dbscan.results.each do |k, arr|
+		unless k==-1
+			avlat = arr.transpose[0].inject{|sum, el| sum+el }.to_f/arr.size
+			avlon = arr.transpose[1].inject{|sum, el| sum+el }.to_f/arr.size
+			avwidth = arr.transpose[2].inject{|sum, el| sum+el }.to_f/arr.size
+			avheight = arr.transpose[3].inject{|sum, el| sum+el }.to_f/arr.size
+			writer << [avlat, avlon, avwidth, avheight]
+		end	
 	end
 end
-puts "DBSCAN omplete"
+puts "DBSCAN complete"
 
+# dbscan.results.each{|k,arr| puts arr.transpose[0].inject{|sum, el| sum+el }.to_f/arr.size if k!=-1}
