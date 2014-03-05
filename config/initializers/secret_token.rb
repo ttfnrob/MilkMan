89fc17bb4617a9a5700f0cb676587c94a91835a0
Robert Simpson <ttfnrob@gmail.com>
2014-03-05 12:10:00 +0000

    Fix

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 MyApp::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
-  ('x' * 30) # meets minimum requirement of 30 chars long
+  "43171864c6b64df56b120c385d20043d776d23df304605623b9a0b7dc58954989d89ff4bde5e4f927cab904272173578b996fbd1a16f9b8be28993fc5a8dcf09" # meets minimum requirement of 30 chars long
 else
   ENV['SECRET_TOKEN']
 end
