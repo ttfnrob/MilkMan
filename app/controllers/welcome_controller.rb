f192254d6aa7a25ee0ba47df90c223d0ad4e58f1
Robert Simpson <ttfnrob@gmail.com>
2014-05-08 14:29:12 +0100

    Better asset caching and warning at 0 preloaded

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,9 @@
 class WelcomeController < ApplicationController
   def index
     @pagetitle = "Milkman"
+    @presubjects = []
+    @newsubjects = []
     @presubjects = Subject.where(:state => "complete").where(:zooniverse_id.ne => "AMW0000v75").where(:cached_annotations.ne => nil).sort(:classification_count.desc).limit(12)
     @newsubjects = Subject.where(:state => "complete").where(:zooniverse_id.ne => "AMW0000v75").where(:cached_annotations => nil).sort(:classification_count.desc).limit(12)
-
-    if Subject.where(:state => "complete").where(:zooniverse_id.ne => "AMW0000v75").where(:cached_annotations.ne => nil).size < 300
-      Subject.where(:state => "complete").where(:cached_annotations => nil).sort(:classification_count.desc).limit(50).each do |i|
-        puts "#{i.zooniverse_id}, #{i.classification_count} #{i.annotations.size}"
-      end
-    end
   end
 end
