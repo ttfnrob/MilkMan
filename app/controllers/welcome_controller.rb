7321ea8e5215603a64812dc1323d3164b8eaeca9
Robert Simpson <ttfnrob@gmail.com>
2014-05-11 21:26:17 +0100

    Fixing preload limit to make it faster when needed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,7 @@ class WelcomeController < ApplicationController
     @newsubjects = Subject.where(:state => "complete").where(:zooniverse_id.ne => "AMW0000v75").where(:cached_annotations => nil).sort(:classification_count.desc).limit(12)
 
     if Subject.where(:state => "complete").where(:zooniverse_id.ne => "AMW0000v75").where(:cached_annotations.ne => nil).size < 50
-      Subject.where(:state => "complete").where(:cached_annotations => nil).sort(:classification_count.desc).limit(50).each do |i|
+      Subject.where(:state => "complete").where(:cached_annotations => nil).sort(:classification_count.desc).limit(6).each do |i|
         puts "#{i.zooniverse_id}, #{i.classification_count} #{i.annotations.size}"
       end
     end
